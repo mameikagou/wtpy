4d7259ad20092f29d49cccec63157f592c100d24
mameikagou <mrlonely1226@163.com>
2026-08-22 12:55:53 +0800

    build: identify qrant wtpy variant

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/wondertrader/wtpy'
 EMAIL = 'silencesword@foxmail.com'
 AUTHOR = 'Wesley Liu'
 REQUIRES_PYTHON = '>=3.8.0'
-VERSION = '0.9.9.3'
+VERSION = '0.9.9.3+qrant.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
